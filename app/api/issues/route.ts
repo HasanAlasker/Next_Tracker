de2a5cdbb193176a219ccc7bbbf66b0651aebee1
Hasan Alasker <hasanalasker2004@gmail.com>
2026-06-08 11:14:24 +0300

    added prisma client, and create issue validation and route

---
+++ app/api/issues/route.ts
@@ -0,0 +1,33 @@
+import { prisma } from "@/app/lib/prisma";
+import { createIssueSchema } from "@/app/validation/issueSchema";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const validation = createIssueSchema.safeParse(body);
+
+    if (!validation.success)
+      return NextResponse.json(
+        { error: validation.error.issues },
+        { status: 400 },
+      );
+
+    const issue = await prisma.issue.create({
+      data: {
+        title: body.title,
+        description: body.description,
+      },
+    });
+
+    if (!issue)
+      return NextResponse.json(
+        { error: "Couldn't create issue" },
+        { status: 400 },
+      );
+
+    return NextResponse.json({ issue }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+}
